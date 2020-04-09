c18ddcc31b345a865f80f69e257424e54a9667c9
root <root@localhost.localdomain>
2020-04-09 17:40:13 +0200

    add layout/template

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,47 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="profile"href="">
-    <link rel="pingback" href="">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <title>BarkeyTokyo</title>
-  </head>
-  <body>
-    <header class="navbar-fixed">
-        <nav>
-          <div class="nav-wrapper">
-            <a href="#" data-target="mobile-demo" class="sidenav-trigger right"><i class="material-icons">menu</i></a>
-            <div class="container">
-              <h1>
-                <a href="#!" class="brand-logo">
-                  <img src="{{ asset('images/logo.png') }}" width="100px" height="auto">
-                </a>
-              <h1>
-              <ul class="right hide-on-med-and-down">
-                <li><a href="sass.html">ログイン</a></li>
-                <li><a href="badges.html">ログアウト</a></li>
-                <li><a href="collapsible.html">新規登録</a></li>
-                <li><a href="mobile.html">設定</a></li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <ul class="sidenav" id="mobile-demo">
-          <li><a href="sass.html">ログイン</a></li>
-          <li><a href="badges.html">ログアウト</a></li>
-          <li><a href="collapsible.html">新規登録</a></li>
-          <li><a href="mobile.html">設定</a></li>
-          <li><a class="sidenav-close" href="#close">閉じる</a></li>
-        </ul>
-    </header>
-    <main>
+@extends('layout.template')
+@section('title', '')
+@section('contents')
       <div id="top-bg" class="valign-wrapper">
         <div class="container row" id="top">
               <div class="col s12">
@@ -240,7 +199,7 @@
             </div>
           </div>
           <div class="tags" id="tags">
-            <h2><i class="material-icons">bookmark</i>タグ</h2>
+            <h2><i class="material-icons">bookmark</i>店名</h2>
             <ul>
               <li><a href="#">Andersen</a></li>
               <li><a href="#">PAUL</a></li>
@@ -292,32 +251,5 @@
               <li><a href="#">デリフランス</a></li>
               <li><a href="#">ジャンフランソワ</a></li>
             </ul>
-          </di>
-    </main>
-    <footer>
-      <div class="footer-inner">
-        <p class="copyright"><small>@Copyright2020 Shiba-WebSite.All Rights Reserved.</small></p>
-      </div>
-    </footer>
-    <script
-      src="https://code.jquery.com/jquery-3.4.1.js"
-      integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-      crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script src="{{ asset('js/jquery.matchHeight-min.js') }}"></script>
-    <script src="{{ asset('js/default.js') }}"></script>
-    <script> 
-    $(function(){
-      $('a[href="#recommend"], a[href="#choosePanFromImage"]').click(function(){
-        var speed = 500;
-        var href= $(this).attr("href");
-        var target = $(href == "#" || href == "" ? 'html' : href);
-        var position = target.offset().top;
-        $("html, body").animate({scrollTop:position}, speed, "swing");
-        return false;
-      });
-      $('#recommend > .col > .recommend-item > figure, #choosePanFromImage > .col > .choosePanFromImage-item > figure').matchHeight();
-    });
-    </script>
-  </body>
-</html>
+          </div>
+@endsection
